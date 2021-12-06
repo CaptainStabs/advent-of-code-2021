36614019a40eee22d6e771cca870c7887eda04ed
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-12-06 09:01:14 -0500

    add tqdm

--- 6_day/1_part.py
+++ 6_day/1_part.py
@@ -1,14 +1,21 @@
-with open("input.txt", "r", newline="", encoding="utf-8") as f:
-    line = f.readline()
-    fish_list = line.split(",")
+from tqdm import tqdm
 
-    for day in range(80):
-        for i in range(len(fish_list)):
-            if fish_list[i] == 0:
-                fish_list[i] = 6
-                fish_list.append(8)
-            else:
-                fish_list[i] = int(fish_list[i]) - 1
-        # print(str("After  {} day:  {}").format(day, fish_list))
+def calculate_fish(days):
+    with open("input.txt", "r", newline="", encoding="utf-8") as f:
+        line = f.readline()
+        fish_list = line.split(",")
 
-    print("Total fish: " + str(len(fish_list)))
+        for day in tqdm(range(days)):
+            for i in range(len(fish_list)):
+                if fish_list[i] == 0:
+                    fish_list[i] = 6
+                    fish_list.append(8)
+                else:
+                    # Couldn't decrement here
+                    fish_list[i] = int(fish_list[i]) - 1
+            # print(str("After  {} day:  {}").format(day, fish_list))
+
+        print("Total fish: " + str(len(fish_list)))
+
+calculate_fish(80)
+calculate_fish(256)
