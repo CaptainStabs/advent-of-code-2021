6ad7eec017c0c6a3c9cba52d4531623aa317401e
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2021-12-07 11:19:58 -0500

    hmm

--- 7_day/1_part.py
+++ 7_day/1_part.py
@@ -1,27 +1,27 @@
+from statistics import mode
+
 with open("input.txt", "r", newline="", encoding="utf-8") as f:
     line = f.read().strip("\r\n")
+
+    # Convert strings to ints
     positions = sorted([int(i) for i in line.split(",")])
-    # print(positions)
+    print(positions)
 
-    p = len(positions)
-    print(p)
-
-    m1 = positions[p//2]
-    m2 = positions[p//2-1]
-    m = m1 + m2 // 2
-    m -= 1
-    print(m)
+    # m = mode(positions)
+    m = 328
+    print("Mode:", m)
 
     fuel_spent = 0
     for i in range(len(positions)):
         if positions[i] == m:
+            print(str("- Move from {} to {}: {} fuel").format(positions[i], m, positions[i] - m))
             continue
         else:
             if positions[i] > m:
                 fuel_spent += positions[i] - m
-                # print(str("- Move from {} to {}: {} fuel").format(positions[i], m, positions[i] - m))
+                print(str("- Move from {} to {}: {} fuel").format(positions[i], m, positions[i] - m))
             else:
                 fuel_spent += m - positions[i]
-                # print(str("- Move from {} to {}: {} fuel").format(positions[i], m, m - positions[i]))
+                print(str("- Move from {} to {}: {} fuel").format(positions[i], m, m - positions[i]))
 
     print("\nTotal fuel spent:", fuel_spent)
